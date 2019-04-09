96856ee2fdc67d1494d373a5be5f34aed0820077
Jordan Hicks <jordan.hicks@uon.edu.au>
2019-04-09 03:58:19 +0000

    tried understanding this and achieved SHIT ALL

--- task1.c
+++ task1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void)
+int main() //encryption with rotation cipher given text and rotation amount
 {
-    printf("hello, world\n");
+    int i;
+    char c = i;
+    for(i >= 65, i <= 90) {
+        char letter[26];
+    }
+    printf("%c", 1);
 }
