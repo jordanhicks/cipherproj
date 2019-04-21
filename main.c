4f28aa5c6937ce86e59eb7e9d7296c6ca01b038c
Jordan Hicks <jordan.hicks@uon.edu.au>
2019-04-21 07:10:33 +0000

    created user interface menu

--- main.c
+++ main.c
@@ -3,15 +3,29 @@
 int main()  {
     
     char text[200]; //message that will be taken
-    int key; //the shiftkey
+    int menuOption; //the options at the initial menu
+    int shiftKey; //the key that the text will be incremented by
     
-    printf("Enter your text: ");
-    scanf("%s", text);
-    printf("%s\n", text);
     
-    printf("Enter your key: ");
-    scanf("%d", &key);
-    printf("%d\n", key);
+    printf("Enter your message: "); 
+    scanf("%s", text); //receiving the message
+    printf("%s\n\n", text);
+    
+    printf("Please select a number from the following options:\n");
+    printf("1 - Caesar Cipher Encryption\n\n");
+    scanf("%d", &menuOption);
+    
+    switch (menuOption) {
+        case 1: //Caesar Cipher Encryption
+            printf("Enter your key: ");
+            scanf("%d", &shiftKey); //receiving the key
+            printf("%d\n", shiftKey);
+            
+            break;
+            
+        default :
+            printf("Invalid menu option.");
+    }
     
     
   return 0;
