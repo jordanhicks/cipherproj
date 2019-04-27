2f2fadfcebf40281a2e0a350b1ed5212d05c90cc
Jordan Hicks <jordan.hicks@uon.edu.au>
2019-04-27 04:37:27 +0000

    added substitution decryption

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main()  {
     int menuOption; //the options at the initial menu
     int shiftkey; //the key that the caesar cipher will be incremented by
     char subkey[200] = "QWERTYUIOPASDFGHJKLZXCVBNM"; //the key that each letter will be replaced with in a substitution cipher
+    char regkey[200] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //regular alphabet key
     char caesar; //encrypted caesar text
     char sub; //encrypted substitution text
     int i; //letter value
@@ -17,7 +18,7 @@ int main()  {
     printf("%s\n\n", text);
     
     printf("Please select a number from the following options:\n");
-    printf("1 - Caesar Cipher Encryption\n2 - Caesar Cipher Decryption\n3 - Substitution Cipher Encryption\n\n");
+    printf("1 - Caesar Cipher Encryption\n2 - Caesar Cipher Decryption\n3 - Substitution Cipher Encryption\n4 - Substitution Cipher Decryption\n\n");
     scanf("%d", &menuOption);
     
     switch (menuOption) {
@@ -82,13 +83,30 @@ int main()  {
 	            if (sub >= 'A' && sub <= 'Z'){
 	                j = sub - 65; //deduct ascii value 
 	                sub = subkey[j]; //initialise the key
-	             }
+	            }
 	            text[i] = sub; //apply encrypted text as output text
 	        }	
 	        printf("Encrypted text: %s", text);
             break;
                 
+        
+        case 4: //substitution cipher decryption
+            printf("You have chosen Substitution Cipher Decryption.\n\n");
+            printf("By default the key is based off the QWERTY alphabet.\n\n");
+            
+            for (i=0; text[i] != '\0'; ++i){
+                sub = text[i]; //make decrypted text same as the input text
                 
+                if (sub >= 'A' && sub <= 'Z'){
+                    j = sub - 65; //deduct ascii value
+                    sub = regkey[j]; //initialise key to revert to standard alphabet
+                }
+                text[i] = sub; //apply decrypted text as output text
+            }
+            printf("Decrypted text: %s", text);
+            break;
+        
+        
         default :
             printf("Invalid menu option.");
     }
